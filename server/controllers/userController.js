87649e16f99e0c0f8cf648891da5ce01e07a7e35
uchikuch <ucheazinge@gmail.com>
2021-04-17 09:01:59 +0100

    Add User Endpoint Done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,5 +2,14 @@ const { user } = require("../models");
 
 module.exports.create_user = async (req, res) => {
   console.log("creating user", req.body);
-  res.status(200).json({ message: "It works" });
+
+  const { first_name, last_name, email } = req.body;
+
+  try {
+    const newUser = await user.create({ first_name, last_name, email });
+    return res.status(200).json(newUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 };
